dffd1a7fcb0e020dd0f83feb73e121c2e7a149b9
chriscotimms <chriscotimms@gmail.com>
2024-01-22 16:44:33 +0000

    added google image search api

---
+++ googleimg.js
@@ -0,0 +1,36 @@
+require("dotenv").config();
+
+// Replace 'YOUR_API_KEY' with your actual API key from the Google Cloud Console.
+const apiKey = process.env.GOOGLE_API_KEY;
+
+// Replace 'YOUR_CX' with your actual Custom Search Engine ID.
+const cx = "73842e1aba003487a";
+
+
+
+const fetchImgSearch = async (searchQuery) => { 
+
+    const apiUrl = `https://www.googleapis.com/customsearch/v1?key=${apiKey}&cx=${cx}&q=${searchQuery}`;
+
+    try {
+    // Make a GET request to the Unsplash API using async/await
+    const response = await fetch(apiUrl);
+
+    // Check if the request was successful (status code 200)
+    if (response.ok) {
+      // Parse the JSON response
+       const data = await response.json();
+      // Access photo information from the data
+      //console.log(data.items[0].pagemap.cse_image[0].src);
+        return data.items;
+    } else {
+      // Handle errors
+      throw new Error(`Error: ${response.status}`);
+    }
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+
+module.exports = { fetchImgSearch }
--- public/index.js
+++ public/index.js
@@ -1,5 +1,8 @@
 
 const form = document.getElementById("input-container");
+const form3 = document.getElementById("search-input-container");
+
+
 
 form.addEventListener("submit", async function(event) {
     event.preventDefault(); // Prevent the form from submitting the traditional way
@@ -23,3 +26,27 @@ form.addEventListener("submit", async function(event) {
 
     console.log("imgUrlInput: " + imgUrlInput);
 });
+
+form3.addEventListener("submit", async function(event) {
+    event.preventDefault(); // Prevent the form from submitting the traditional way
+  
+
+    var searchInput = document.getElementById("search-input").value;
+    
+    // console.log(JSON.stringify({imgUrlInput}));
+    
+    const response = await fetch("/image-search",{
+        method:"POST",
+        headers: {
+            "content-type": "application/json"
+        },
+        body: JSON.stringify({searchInput}),
+    });
+    if (response.ok) {
+        const result = await response.json();
+        console.log(result);
+    };
+
+    console.log("searchInput: " + searchInput);
+});
+
