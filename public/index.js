c61170d5fc495439ae56e075b315594154f6d2d4
Edga380 <a.edgaras44@gmail.com>
2024-01-23 11:53:02 +0000

    Html/css refactoring/new features

--- public/index.js
+++ public/index.js
@@ -1,73 +1,127 @@
-
-const form = document.getElementById("input-container");
-const form3 = document.getElementById("search-input-container");
+const form1 = document.getElementById("input-container");
 const form2 = document.getElementById("input-container2");
+const form3 = document.getElementById("search-input-container");
 
-form.addEventListener("submit", async function(event) {
-    event.preventDefault(); // Prevent the form from submitting the traditional way
-  
-    let imgUrlInput = document.getElementById("imgUrlInput").value;
-    let jsonInput = JSON.stringify({imgUrlInput});
+const header = document.getElementById("header-id");
 
-    // console.log(JSON.stringify({imgUrlInput}));
-    
-    const response = await fetch("/submitUrl",{
-        method:"POST",
-        headers: {
-            "content-type": "application/json"
-        },
-        body: JSON.stringify({imgUrlInput}),
-    });
-    if (response.ok) {
-        const result = await response.json();
-        console.log(result);
-    };
-    console.log("imgUrlInput: " + imgUrlInput);
+// Form 1 Input url
+form1.addEventListener("submit", async function (event) {
+  event.preventDefault();
+
+  let imgUrlInput = document.getElementById("imgUrlInput").value;
+
+  const response = await fetch("/submitUrl", {
+    method: "POST",
+    headers: {
+      "content-type": "application/json",
+    },
+    body: JSON.stringify({ imgUrlInput }),
+  });
+  if (response.ok) {
+    const result = await response.json();
+    const audioContainer = document.getElementById("audio-container");
+    if (audioContainer.firstElementChild) {
+      audioContainer.removeChild(audioContainer.firstElementChild);
+    }
+    audioContainer.insertAdjacentHTML(
+      "beforeend",
+      `
+      <audio controls>
+        <source src="${result.speechData[1]}.mp3" type="audio/mp3" />
+        Your browser does not support the audio element.
+      </audio>
+      `
+    );
+    console.log(result);
+  }
+  console.log("imgUrlInput: " + imgUrlInput);
 });
 
+// Form 2
+form2.addEventListener("submit", async function (event) {
+  event.preventDefault();
 
+  let imgUrlInput = document.getElementById("submitUrltoscrape").value;
 
-form2.addEventListener("submit", async function(event) {
-    event.preventDefault(); // Prevent the form from submitting the traditional way
-  
-    let imgUrlInput = document.getElementById("submitUrltoscrape").value;
-    let jsonInput = JSON.stringify({imgUrlInput});
-
-    // console.log(JSON.stringify({imgUrlInput}));
-    
-    const response = await fetch("/submitUrltoscrape",{
-        method:"POST",
-        headers: {
-            "content-type": "application/json"
-        },
-        body: JSON.stringify({imgUrlInput}),
-    });
-    if (response.ok) {
-        const result = await response.json();
-        console.log(result);
-    };
-    console.log("imgUrlInput: " + imgUrlInput);
+  const response = await fetch("/submitUrltoscrape", {
+    method: "POST",
+    headers: {
+      "content-type": "application/json",
+    },
+    body: JSON.stringify({ imgUrlInput }),
+  });
+  if (response.ok) {
+    const result = await response.json();
+    console.log(result);
+  }
 });
 
-form3.addEventListener("submit", async function(event) {
-    event.preventDefault(); // Prevent the form from submitting the traditional way
-  
+// Form 3
+form3.addEventListener("submit", async function (event) {
+  event.preventDefault();
 
-    var searchInput = document.getElementById("search-input").value;
-    
-    // console.log(JSON.stringify({imgUrlInput}));
-    
-    const response = await fetch("/image-search",{
-        method:"POST",
-        headers: {
-            "content-type": "application/json"
-        },
-        body: JSON.stringify({searchInput}),
-    });
-    if (response.ok) {
-        const result = await response.json();
-        console.log(result);
-    };
+  let searchInput = document.getElementById("search-input").value;
 
-    console.log("searchInput: " + searchInput);
+  const response = await fetch("/image-search", {
+    method: "POST",
+    headers: {
+      "content-type": "application/json",
+    },
+    body: JSON.stringify({ searchInput }),
+  });
+
+  if (response.ok) {
+    const result = await response.json();
+    const imagesContainer = document.querySelector(".images-container");
+    while (imagesContainer.firstChild) {
+      imagesContainer.removeChild(imagesContainer.firstChild);
+    }
+    console.log(result.imgResultsArray);
+    for (let i = 0; i < result.imgResultsArray.length; i++) {
+      imagesContainer.insertAdjacentHTML(
+        "beforeend", // Use a specific string for the position argument
+        `
+            <img class="searchImages"
+              src="${
+                result.imgResultsArray[i].pagemap.cse_image[0].src === undefined
+                  ? "https://upload.wikimedia.org/wikipedia/commons/thumb/6/65/No-Image-Placeholder.svg/832px-No-Image-Placeholder.svg.png"
+                  : result.imgResultsArray[i].pagemap.cse_image[0].src
+              }"
+              alt=""
+            />
+        `
+      );
+    }
+    console.log(result.imgResultsArray);
+  }
 });
+
+// OnLoad run some functions
+window.onload = () => {
+  RetrieveHeaderBgImg();
+};
+
+// Retrieve a random image from unsplash api to display as header bg
+async function RetrieveHeaderBgImg() {
+  const response = await fetch("/retrieveBgImage", {
+    method: "POST",
+    headers: {
+      "content-type": "application/json",
+    },
+  });
+
+  if (response.ok) {
+    const data = await response.json();
+    header.style.backgroundImage = `url(${data.imageUrl})`;
+    console.log(data);
+  }
+}
+
+// Switches between options
+function SwitchBetweenOptions(index) {
+  const sections = document.querySelectorAll(".section");
+  for (let i = 0; i < sections.length; i++) {
+    sections[i].classList.remove("active");
+  }
+  sections[index].classList.add("active");
+}
--- server.js
+++ server.js
@@ -2,42 +2,38 @@ require("dotenv").config();
 const { visionRequest } = require("./openai-vision");
 const { speech } = require("./openai-texttospeach");
 const { fetchImgSearch } = require("./googleimg");
-const { scrapeImages  } = require("./htmlparser");
+const { scrapeImages } = require("./htmlparser");
+const { FetchRandomPhotoForBg } = require("./randombgimg");
 
 const express = require("express");
 
 const app = express();
 
+const port = 3000;
+
 app.use(express.static("public"));
-
-
 
 app.post("/submitUrl", express.json(), async (req, res) => {
   try {
     console.log(req.body.imgUrlInput);
 
     const visionResult = await visionRequest(req.body.imgUrlInput);
-    // console.log(visionResult.message.content);
-    const speechfile = await speech(visionResult.message.content);
-    console.log("within server:", speechfile);
+    const speechData = await speech(visionResult.message.content);
 
-    res.json({speechfile:speechfile});
-  } catch(error) {
+    res.json({ speechData: speechData });
+  } catch (error) {
     console.error("error found:", error);
-  };
+  }
 });
 
 app.post("/image-search", express.json(), async (req, res) => {
   try {
-    // console.log(req.body.searchImage);
-    const imgresultsArray = await fetchImgSearch(req.body.searchImage);
-    // console.log(imgresultsArray);
-    res.json({imgresultsArray:imgresultsArray});
-  } catch(error) {
+    const imgResultsArray = await fetchImgSearch(req.body.searchImage);
+    res.json({ imgResultsArray: imgResultsArray });
+  } catch (error) {
     console.error("error found:", error);
-  };
+  }
 });
-
 
 app.post("/submitUrltoscrape", express.json(), async (req, res) => {
   try {
@@ -46,23 +42,26 @@ app.post("/submitUrltoscrape", express.json(), async (req, res) => {
     const scrapedImgUrlArray = await scrapeImages(req.body.imgUrlInput);
     console.log(scrapedImgUrlArray);
 
-
     const visionResult = await visionRequest(scrapedImgUrlArray[0]);
-     console.log(visionResult.message.content);
-  //   const speechfile = await speech(visionResult.message.content);
-  //   console.log("within server:", speechfile);
+    console.log(visionResult.message.content);
+    //   const speechfile = await speech(visionResult.message.content);
+    //   console.log("within server:", speechfile);
 
-  //   res.json({speechfile:speechfile});
-   } catch(error) {
-     console.error("error found:", error);
-   };
+    //   res.json({speechfile:speechfile});
+  } catch (error) {
+    console.error("error found:", error);
+  }
 });
 
+app.post("/retrieveBgImage", express.json(), async (req, res) => {
+  try {
+    const imgUrl = await FetchRandomPhotoForBg();
 
-
-
-
-const port = 3000;
+    res.json({ success: true, imageUrl: imgUrl });
+  } catch (error) {
+    console.error("error found:", error);
+  }
+});
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
